d2d3ca70c656be346c9f3ad120c1bb593478496e
narek88888 <gabrelyannarek1@gmail.com>
2025-07-31 13:40:54 +0400

    added experiments

---
+++ src/experiments/experiments1.java
@@ -0,0 +1,26 @@
+package experiments;
+
+public class experiments1 {
+
+
+    public static void main(String[] args) {
+            int i = 0;
+            print(i);
+            System.out.println(i);
+        }
+
+    public static void print(int i) {
+        i = 8;
+        System.out.println(i);
+    }
+
+
+
+
+
+
+
+
+}
+
+
---
+++ src/experiments/experiments2.java
@@ -0,0 +1,31 @@
+package experiments;
+
+/*
+public class experiments2 {
+
+ int i = 8;
+    {
+        int i = 30;
+
+    }
+
+    public static void test(){
+        int i = 11;
+
+    }
+
+    public experiments2(){
+        int i = 20;
+
+    }
+
+    public static void main(String[] args){
+        System.out.println(i);
+        System.out.println(test());
+    }
+
+
+}
+
+
+ */
